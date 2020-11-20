81f89475bcee2e2f62ede2c38970ec0923bcc5e2
Ryan Shintaku <ryanshintaku@Ryans-MacBook-Pro.local>
2020-11-20 14:26:47 -0800

    testing how to use structs

--- test.ex
+++ test.ex
@@ -1,8 +1,23 @@
+#IO.puts "Hello, world!"
+#defmodule Product do
+#  defstruct val: "Hello"
+#end
+#defmodule Main do
+##  def main() do
+#   test = %Product{}
+#    IO.puts(test.val)
+#  end
+#end
 
-
-
-IO.puts "Hello, world!"
-defmodule Product do
-  defstruct val: "Hello"
+defmodule City do
+  defstruct name: "San Framcisco", state: "CA"
 end
 
+defmodule Main do
+  def main do
+    test = %City{}
+    IO.puts(test.name)
+  end
+end
+
+Main.main
