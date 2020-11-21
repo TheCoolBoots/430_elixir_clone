dbe783cebf0f7bbe6eb9a60f38e21110df79050c
Andrew Lai <arjlai221@gmail.com>
2020-11-20 17:14:35 -0800

    primitive template implemented

--- DXUQ4.ex
+++ DXUQ4.ex
@@ -1,5 +1,5 @@
 defmodule IdC do
-  defstruct [:num]
+  defstruct [:id]
 end
 
 defmodule AppC do
@@ -37,36 +37,82 @@ end
 
 defmodule M do
 
-	anonFunc = fn () -> IO.puts "passed function" end
-
-	def interp(expr, _env) do
+	def interp(expr, env) do
 		cond do
 			(expr.__struct__ == NumV) -> expr
 			(expr.__struct__ == StrV) -> expr
 			(expr.__struct__ == BoolV) -> expr
 			(expr.__struct__ == PrimV) -> 1
 			(expr.__struct__ == CloV)-> 1
-			(expr.__struct__ == IdC) -> 1
-			(expr.__struct__ == AppC) -> 1
+			(expr.__struct__ == IdC) -> lookup_env(env, expr.id)
+			(expr.__struct__ == AppC) ->
+				interpretedBody = interp(expr.body, env)
+				cond do
+					length(expr.args) > 2 -> "ERROR: invalid number of arguments"
+					true ->
+						[firstEl|rest] = expr.args
+						second = List.first(rest)
+						a = interp(firstEl, env)
+						b = interp(second, env)
+						IO.puts interpretedBody.(a, b).val
+						# interpretedBody.(a, b)
+				end
 			(expr.__struct__ == CondC) -> 1
 			(expr.__struct__ == LamC) -> 1
 			true -> "ERROR"
 		end
 	end
 
-	def test(func) do
-		func.()
+	def init_interp(ast) do
+		bindAdd = {:+,  fn (a, b) ->
+											cond do
+												(a.__struct__ == NumV && b.__struct__ == NumV) -> %NumV{val: (a.val + b.val)}
+												true -> "ERROR: invalid operands for +"
+											end
+										end}
+		bindSub = {:-, "sub"}
+		bindMult = {:*, "mult"}
+		bindDiv = {:/, "div"}
+		bindLeq = {:leq, "leq"}
+		bindEqual = {:equal?, "equal?"}
+		top_env = [bindAdd, bindSub, bindMult, bindDiv, bindLeq, bindEqual]
+
+		# IO.puts lookup_env(top_env, :+)
+
+		interp(ast, top_env)
+	end
+
+	def lookup_env(env, target) do
+		[first|rest] = env
+		cond do
+			length(env) == 0 -> "ERROR: symbol not found"
+			elem(first, 0) == target -> elem(first, 1)
+			true -> lookup_env(rest, target)
+		end
 	end
 
 	def main() do
-
-		test1 = %StrV{val: "magic"}
-		IO.puts(test1.val)
-		test(anonFunc)
+		testId = %IdC{id: :+}
+		a = %NumV{val: 1}
+		b = %NumV{val: 3}
+		testAppC = %AppC{body: testId, args: [a, b]}
+		init_interp(testAppC)
 	end
 
 end
 
+
 M.main()
 
+# anonFunc = fn () -> IO.puts "passed function" end
+# def main() do
+# 	test1 = %StrV{val: "magic"}
+# 	IO.puts(test1.val)
+# 	test(M.anonFunc)
+# end
 # newCHANGE
+
+
+# def test(func) do
+# 	func.()
+# end
