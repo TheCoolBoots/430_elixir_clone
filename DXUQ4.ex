12bf587b2802011d02787cad20e9bd2a5c102075
Andrew Lai <arjlai221@gmail.com>
2020-11-23 10:22:50 -0800

    interp done

--- DXUQ4.ex
+++ DXUQ4.ex
@@ -10,6 +10,10 @@ defmodule LamC do
   defstruct [:body, :params]
 end
 
+defmodule CondC do
+	defstruct [:if, :then, :else]
+end
+
 defmodule NumV do
 	@enforce_keys [:val]
 	defstruct [:val]
@@ -42,39 +46,109 @@ defmodule M do
 			(expr.__struct__ == NumV) -> expr
 			(expr.__struct__ == StrV) -> expr
 			(expr.__struct__ == BoolV) -> expr
-			(expr.__struct__ == PrimV) -> 1
-			(expr.__struct__ == CloV)-> 1
 			(expr.__struct__ == IdC) -> lookup_env(env, expr.id)
 			(expr.__struct__ == AppC) ->
 				interpretedBody = interp(expr.body, env)
 				cond do
-					length(expr.args) > 2 -> "ERROR: invalid number of arguments"
-					true ->
-						[firstEl|rest] = expr.args
-						second = List.first(rest)
-						a = interp(firstEl, env)
-						b = interp(second, env)
-						IO.puts interpretedBody.(a, b).val
-						# interpretedBody.(a, b)
+					interpretedBody.__struct__ == PrimV ->
+						cond do
+							length(expr.args) > 2 -> "ERROR: invalid number of arguments"
+							true ->
+								[firstEl|rest] = expr.args
+								second = List.first(rest)
+								a = interp(firstEl, env)
+								b = interp(second, env)
+								IO.puts interpretedBody.func.(a, b).val
+								# interpretedBody.(a, b)
+						end
+					interpretedBody.__struct__ == CloV ->
+						newIds = interpretedBody.params
+						interpretedArgs = Enum.map(expr.args, fn (arg) -> interp(arg, env) end)
+						newEnv = extendEnv(newIds, interpretedArgs, env)
+						interp(interpretedBody.body, newEnv)
+					true -> "ERROR: applied arguments to non-function"
 				end
-			(expr.__struct__ == CondC) -> 1
-			(expr.__struct__ == LamC) -> 1
+			(expr.__struct__ == CondC) -> interpCond(interp(expr.if), expr.then, expr.else)
+			(expr.__struct__ == LamC) -> %CloV{body: expr.body, params: expr.params, clo_env: env}
 			true -> "ERROR"
 		end
 	end
 
+
+	def interpCond(boolean, ifTrue, ifElse) do
+		cond do
+			boolean.__struct__ == BoolV ->
+				cond do
+					boolean.val -> interp(ifTrue)
+					true -> interp(ifElse)
+				end
+			true -> "ERROR: if statement is not a boolean"
+		end
+	end
+
+
+	def extendEnv(ids, args, oldEnv) do
+		cond do
+			(length(ids) == 0 and !(length(args) == 0)) or (!(length(ids) == 0) and length(args) == 0) -> "ERROR: uneven numbers of ids and args"
+			length(ids) == 0 -> oldEnv
+			true ->
+				[firstId|restIds] = ids
+				[firstArg|restArgs] = args
+				newEnv = List.insert_at(oldEnv, 0, {firstId, firstArg})
+				IO.puts firstId
+				IO.puts firstArg
+				extendEnv(restIds, restArgs, newEnv)
+		end
+	end
+
+
+	def testExtendEnv() do
+		ids = [:a, :b, :c]
+		args = [1, 2, 3]
+		newEnv = extendEnv(ids, args, [])
+		# IO.puts List.first(newEnv)
+	end
+
+
 	def init_interp(ast) do
-		bindAdd = {:+,  fn (a, b) ->
+		bindAdd = {:+,  %PrimV{func: fn (a, b) ->
 											cond do
 												(a.__struct__ == NumV && b.__struct__ == NumV) -> %NumV{val: (a.val + b.val)}
 												true -> "ERROR: invalid operands for +"
 											end
-										end}
-		bindSub = {:-, "sub"}
-		bindMult = {:*, "mult"}
-		bindDiv = {:/, "div"}
-		bindLeq = {:leq, "leq"}
-		bindEqual = {:equal?, "equal?"}
+										end}}
+		bindSub = {:-, %PrimV{func: fn (a, b) ->
+											cond do
+												(a.__struct__ == NumV && b.__struct__ == NumV) -> %NumV{val: (a.val - b.val)}
+												true -> "ERROR: invalid operands for -"
+										    end
+										end}}
+		bindMult = {:*, %PrimV{func: fn (a, b) ->
+											cond do
+												(a.__struct__ == NumV && b.__struct__ == NumV) -> %NumV{val: (a.val * b.val)}
+												true -> "ERROR: invalid operands for *"
+											end
+										end}}
+		bindDiv = {:/, %PrimV{func: fn (a, b) ->
+											cond do
+												(b.__struct__ == NumV && b.val == 0) -> "ERROR: dividing by 0"
+												(a.__struct__ == NumV && b.__struct__ == NumV) -> %NumV{val: (a.val / b.val)}
+												true -> "ERROR: invalid operands for /"
+											end
+										end}}
+		bindLeq = {:leq, %PrimV{func: fn (a, b) ->
+											cond do
+												(a.__struct__ == NumV && b.__struct__ == NumV) -> %BoolV{val: (a.val <= b.val)}
+												true -> "ERROR: invalid operands for leq"
+											end
+										end}}
+		bindEqual = {:equal?, %PrimV{func: fn (a, b) ->
+											cond do
+												((a.__struct__ != PrimV || a.__struct__ != CloV) && (b.__struct__ != PrimV || b.__struct__ != CloV)) ->
+													%BoolV{val: (a.val == b.val)}
+												true -> %BoolV{val: false}
+											end
+										end}}
 		top_env = [bindAdd, bindSub, bindMult, bindDiv, bindLeq, bindEqual]
 
 		# IO.puts lookup_env(top_env, :+)
@@ -92,9 +166,9 @@ defmodule M do
 	end
 
 	def main() do
-		testId = %IdC{id: :+}
-		a = %NumV{val: 1}
-		b = %NumV{val: 3}
+		testId = %IdC{id: :/}
+		a = %NumV{val: 4}
+		b = %NumV{val: 2}
 		testAppC = %AppC{body: testId, args: [a, b]}
 		init_interp(testAppC)
 	end
@@ -102,7 +176,7 @@ defmodule M do
 end
 
 
-M.main()
+M.testExtendEnv()
 
 # anonFunc = fn () -> IO.puts "passed function" end
 # def main() do
